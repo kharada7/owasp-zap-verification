e26297640c438b2279fe67613a78d22f63a51bda
Kaori ISHIKAWA <kaori.ishikawa@biprogy.com>
2026-06-04 16:08:19 +0900

    いったんplaywrightのテスト内容を変更

--- tests/juice-shop.spec.ts
+++ tests/juice-shop.spec.ts
@@ -1,29 +1,17 @@
 import { test, chromium } from '@playwright/test';
 
-test('juice shop crawl', async () => {
-  const browser = await chromium.launch({
-    proxy: {
-      server: 'http://127.0.0.1:8080'
-    }
-  });
+test('juice shop crawl', async ({ page }) => {
 
-  const context = await browser.newContext({
-    ignoreHTTPSErrors: true
+  page.on('console', msg => {
+    console.log(msg.text());
   });
-
-  const page = await context.newPage();
 
   await page.goto('http://localhost:3000');
 
-  await page.click('#navbarAccount');
-  await page.click('#navbarLoginButton');
+  console.log(await page.title());
 
-  await page.fill('#email', 'demo@juice-sh.op');
-  await page.fill('#password', 'demo');
-
-  await page.click('#loginButton');
-
-  await page.goto('http://localhost:3000/#/search');
-
-  await browser.close();
+  await page.screenshot({
+    path: 'first-page.png',
+    fullPage: true
+  });
 });
