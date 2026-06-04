adddbb1f77f3899931c0e5956f7958ef71d80494
Kaori ISHIKAWA <kaori.ishikawa@biprogy.com>
2026-06-04 16:29:18 +0900

    yamlを修正

--- tests/juice-shop.spec.ts
+++ tests/juice-shop.spec.ts
@@ -1,17 +1,42 @@
-import { test, chromium } from '@playwright/test';
+import { test, chromium } from "@playwright/test";
 
-test('juice shop crawl', async ({ page }) => {
-
-  page.on('console', msg => {
+test("juice shop crawl", async ({ page }) => {
+  page.on("console", (msg) => {
     console.log(msg.text());
   });
 
-  await page.goto('http://localhost:3000');
+  await page.goto("http://localhost:3000");
 
   console.log(await page.title());
 
   await page.screenshot({
-    path: 'first-page.png',
-    fullPage: true
+    path: "first-page.png",
+    fullPage: true,
   });
+
+  const browser = await chromium.launch({
+    proxy: {
+      server: "http://127.0.0.1:8080",
+    },
+  });
+
+  const context = await browser.newContext({
+    ignoreHTTPSErrors: true,
+  });
+
+  page = await context.newPage();
+
+  await page.goto("http://localhost:3000");
+
+  await page.click("#navbarAccount");
+  await page.click("#navbarLoginButton");
+
+  await page.fill("#email", "demo@juice-sh.op");
+  await page.fill("#password", "demo");
+
+  await page.click("#loginButton");
+
+  await page.goto("http://localhost:3000/#/search");
+
+  await browser.close();
 });
