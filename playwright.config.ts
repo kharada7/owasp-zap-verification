acac724e1a3809daaed66fd8ae5b31a2601ed112
Kaori ISHIKAWA <kaori.ishikawa@biprogy.com>
2026-06-16 13:26:02 +0900

    playwright の設定ファイルと YAML を修正

--- playwright.config.ts
+++ playwright.config.ts
@@ -28,6 +28,11 @@ export default defineConfig({
     /* Base URL to use in actions like `await page.goto('')`. */
     // baseURL: 'http://localhost:3000',
 
+    /* Proxy settings for ZAP */
+    proxy: {
+      server: 'http://127.0.0.1:8080',
+    },
+
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
   },
